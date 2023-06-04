863839542efdba2a18f4a8a337782eb13530d8ba
kinshuk-code-1729 <kinshuk2002.king@gmail.com>
2023-06-04 15:09:39 +0530

    Module added

---
+++ 1.24-Module/module-1.js
@@ -0,0 +1,3 @@
+export let x = 18;
+export let sum = () => 81 + 21;
+export function pdt(){return 17*6};
---
+++ 1.24-Module/module-2.js
@@ -0,0 +1,5 @@
+import { x,sum,pdt } from "./module-1.js";
+
+console.log(x);
+console.log(sum());
+console.log(pdt());
